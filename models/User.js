938f0da1389807241e8e5480c6aa0fac8796e652
daisyisibor <daisyisibor8@gmail.com>
2024-05-22 16:35:39 -0400

    added model files for user,thought& reaction, iincluded a mongoose userschema on user model

---
+++ models/User.js
@@ -0,0 +1,38 @@
+// models/User.js
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+},
+email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Please enter a valid email address']
+},
+thoughts: [
+    {
+    type: Schema.Types.ObjectId,
+    ref: 'Thought'
+    }
+],
+friends: [
+    {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+    }
+]
+});
+
+userSchema.virtual('friendCount').get(function() {
+return this.friends.length;
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
